db6c4971fe211dd62207e1cc664863e8ffdbb639
30xxx30 <mrfunny8500@gmail.com>
2025-02-23 10:14:08 +0530

    Update help.py

--- Taanya/plugins/bot/help.py
+++ Taanya/plugins/bot/help.py
@@ -8,8 +8,7 @@ from Taanya.utils.decorators.language import LanguageStart, languageCB
 from Taanya.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
-from Taanya.utils.stuffs.buttons import BUTTONS
-from Taanya.utils.stuffs.helper import Helper
+
 
 @app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
