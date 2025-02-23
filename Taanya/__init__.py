32d2e026a23283fcf51e392be215a6738d5fe21b
30xxx30 <mrfunny8500@gmail.com>
2025-02-23 09:19:54 +0530

    Add files via upload

---
+++ Taanya/__init__.py
@@ -0,0 +1,30 @@
+from Taanya.core.bot import Taanya
+from Taanya.core.dir import dirr
+from Taanya.core.git import git
+from Taanya.core.userbot import Userbot
+from Taanya.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Taanya()
+userbot = Userbot()
+api = SafoneAPI()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+APP = "ll_DRAGON_MUSIC_BOT"  # connect music api key "Dont change it"
